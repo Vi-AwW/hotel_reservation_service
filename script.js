65e7254156b165c1d0de5173b88e46c1967c9a0c
Vi-AwW <ist23-v.afanasieva@nubip.edu.ua>
2024-07-17 16:21:05 +0300

    result

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const checkin = document.getElementById('checkin').value;
+    const checkout = document.getElementById('checkout').value;
+    const roomType = document.getElementById('room-type').value;
+    
+    const confirmationMessage = `
+        Дякуємо, ${name}! Ваше бронювання підтверджено.
+        Інформація:
+        - Email: ${email}
+        - Дата заїзду: ${checkin}
+        - Дата виїзду: ${checkout}
+        - Тип номера: ${roomType}
+    `;
+    
+    document.getElementById('confirmation-message').innerText = confirmationMessage;
+    document.getElementById('booking-form').style.display = 'none';
+    document.getElementById('booking-confirmation').style.display = 'block';
+});
